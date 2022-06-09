2de012dca5e954d8c22273b055c54deec109ccdb
Dima <1@1>
2022-06-09 15:48:54 +0300

    base logic

--- deposite-service/src/main/java/com/dimastik/deposite/DepositApplication.java
+++ deposite-service/src/main/java/com/dimastik/deposite/DepositApplication.java
@@ -1,15 +1,15 @@
-package com.dimastik.gateway;
+package com.dimastik.deposite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
-public class GatewayApplication {
+@EnableFeignClients
+public class DepositApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(DepositApplication.class, args);
     }
 }
--- deposite-service/src/main/java/com/dimastik/deposite/DepositeApplication.java
+++
@@ -1,7 +0,0 @@
-package com.dimastik.deposite;
-
-public class DepositeApplication {
-    public static void main(String[] args) {
-
-    }
-}
